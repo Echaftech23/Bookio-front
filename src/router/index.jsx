cb054c65ceeb37a7a22e0e5f811cdd6c2799783a
Rachid Echafai <rachidchaf2001@gmail.com>
2024-12-25 12:24:52 +0100

    update project title to 'Bookio Store'; add react-slick and slick-carousel dependencies; enhance CSS with utility classes and animations; modify routing structure for improved navigation

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,16 +3,17 @@ import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import Dashboard from "../pages/Dashboard";
 import Home from "../pages/Home";
-import Layout from "../components/layout/MainLayout";
+import MainLayout from '../components/layout/MainLayout';
 
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/dashboard" element={<Dashboard />} />
+
+      <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
-const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
       colors: {
-        cyan: {
-          50: "#fff1f2",
-          100: "#ffe4e6",
-          200: "#fecdd3",
-          300: "#fda4af",
-          400: "#fb7185",
-          500: "#f43f5e",
-          600: "#e11d48",
-          700: "#be123c",
-          800: "#9f1239",
-          900: "#881337",
+        primary: "#1182c5",
+        secondary: "#2aa6df",
+        dark: "#1e1e1e",
+        light: "#f5f5f5",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
         },
+      },
+      animation: {
+        "spin-slow": "spin 40s linear infinite",
       },
     },
   },
-  plugins: [
-    flowbite.plugin(),
-  ],
-}
+  plugins: [],
+};
